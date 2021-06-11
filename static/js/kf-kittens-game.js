0da78ac2af913bc853464e206a19cd877345b8bf
zhaolinxu <zhaolinxu@sina.cn>
2021-06-11 19:20:56 +0800

    更新

--- static/js/kf-kittens-game.js
+++ static/js/kf-kittens-game.js
@@ -7,7 +7,7 @@ $(function(){
     content +='<div id="close_im" class="close-im"><a href="javascript:void(0);" title="点击关闭">&nbsp;</a></div>';
     content +='<a href="https://qm.qq.com/cgi-bin/qm/qr?k=tCJDY9A6mYcHFtdu13CrU8OqepUIwR-j&jump_from=webapi" target="_blank" class="im-qq qq-a" title="点击加入 - 猫国建设者交流群:499256691">';
     content +='<div class="qq-container"></div>';
-    content +='<div class="qq-hover-c"><img class="img-qq" src="../images/kf/qq.png"></div>';
+    content +='<div class="qq-hover-c"><img class="img-qq" src="https://cdn.jsdelivr.net/gh/g8hh/g8hh.github.io/static/images/kf/qq.png"></div>';
     content +='<span>点击加群</span>';
     content +='</a>';
     content +='<div class="im-tel">';
@@ -23,7 +23,7 @@ $(function(){
     //content +='<img class="weixing-ma" src="//likexia.gitee.io/game/images/kf/zfb-ma.jpg">';
 //    content +='<div class="weixing-txt" style="font-size:13px;">打开手机QQ<br>扫码加群<br>一起讨论吧<br><a href="http://shang.qq.com/wpa/qunwpa?idkey=d10d784492d5e73cca25ff48f4763bdc14f698ffdbf42adbb0e9304495cb0387" target="_blank" title="点击加入 - GitHub游戏交流群:627141737">点击加群</a></div>';
     content +='<div class="weixing-txt" style="font-size:13px;">打开微信扫一扫<br>关注公众号<br><br><a href="http://shang.qq.com/wpa/qunwpa?idkey=d10d784492d5e73cca25ff48f4763bdc14f698ffdbf42adbb0e9304495cb0387" target="_blank" title="点击加入 - Git游戏交流群:627141737">Git游戏</a></div>';
-    content +='<img class="weixing-ma" src="../images/kf/weixing-ma.jpg">';
+    content +='<img class="weixing-ma" src="https://cdn.jsdelivr.net/gh/g8hh/g8hh.github.io/static/images/kf/weixing-ma.jpg">';
     content +='<div class="weixing-sanjiao"></div>';
     content +='<div class="weixing-sanjiao-big"></div>';
     content +='</div>';
