994b7140db6704e0f3685ae7f90560a0323c2906
zhaolinxu <zhaolinxu@sina.cn>
2022-11-07 13:57:56 +0800

    gx

--- static/js/kf.js
+++ static/js/kf.js
@@ -5,7 +5,8 @@ $(function(){
     content +='<div id="open_im" class="open-im">&nbsp;</div><div class="close-ball">x</div>';
     content +='<div class="im_main" id="im_main">';
     content +='<div id="close_im" class="close-im"><a href="javascript:void(0);" title="点击关闭">&nbsp;</a></div>';
-    content +='<a href="https://qm.qq.com/cgi-bin/qm/qr?k=B1c6jzfLBTtNdim1peh9fSWdaOvvzo_S&jump_from=webapi" target="_blank" class="im-qq qq-a" title="点击加入 - Git游戏交流群:627141737">';
+    // content +='<a href="https://qm.qq.com/cgi-bin/qm/qr?k=B1c6jzfLBTtNdim1peh9fSWdaOvvzo_S&jump_from=webapi" target="_blank" class="im-qq qq-a" title="点击加入 - Git游戏交流群:627141737">';
+    content +='<a href="https://qm.qq.com/cgi-bin/qm/qr?k=AnZJbnsU8BsmQGGEWs4rXd-JuSJI-j9c&jump_from=webapi" target="_blank" class="im-qq qq-a" title="点击加入 - Git游戏交流群:793344327">';
     content +='<div class="qq-container"></div>';
     content +='<div class="qq-hover-c"><img class="img-qq" src="//g8hh.com/static/images/kf/qq.png"></div>';
     content +='<span>点击加群</span>';
